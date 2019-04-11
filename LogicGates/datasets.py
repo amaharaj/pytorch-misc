510556a2f310d651e24be6a273085e4f33391dfc
amaharaj <amber.maharaj@uoit.net>
2019-04-11 17:08:56 -0400

    Added other logic gates and tidied up plots

--- LogicGates/datasets.py
+++ LogicGates/datasets.py
@@ -1,5 +1,6 @@
 import torch
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 import numpy as np
 
 class Dataset:
@@ -30,13 +31,32 @@ class Dataset:
 
       value = torch.empty((self.nsamples,1), dtype=torch.int32) # initialize tensor for truth values
 
-      # Assign a value of +1 or -1 based on XNOR rules      
+      # Assign a value of +1 or -1 based on gate type
+      
       if self.kind == "XNOR":
          for n in range(self.nsamples):
-            value[n] = ((A[n]>0) and (B[n]>0)) or (not (A[n]>0) and not(B[n]>0))*2 - 1
-      else:
-            print("Invalid input")
-      
+            value[n] = int(((A[n]>0) and (B[n]>0)) or (not(A[n]>0) and not(B[n]>0)) )*2 - 1
+
+      elif self.kind == "XOR":
+         for n in range(self.nsamples):
+            value[n] = int(((A[n]>0) and (B[n]<0)) or ((A[n]<0) and (B[n]>0)))*2 - 1
+
+      elif self.kind == "NOR":
+         for n in range(self.nsamples):
+            value[n] = int((not (A[n]>0) and not (B[n]>0)))*2 - 1
+
+      elif self.kind == "OR":
+         for n in range(self.nsamples):
+            value[n] = int((not (A[n]>0) and not (B[n]>0)) or (not (A[n]>0) and (B[n]>0) ) or ( (A[n]>0) and not (B[n]>0) ) )*2 - 1
+
+      elif self.kind == "NAND":
+         for n in range(self.nsamples):
+            value[n] = int((not (A[n]>0) and not (B[n]>0)) or (not (A[n]>0) and (B[n]>0) ) or ( (A[n]>0) and not (B[n]>0) ) )*2 - 1
+
+      elif self.kind == "AND":
+         for n in range(self.nsamples):
+            value[n] = int( (A[n]>0) and (B[n]>0)) *2 - 1 
+
       return value
 
    def plot(self):
@@ -48,18 +68,17 @@ class Dataset:
       value = self.Y_data(X)
 
       # Plot the results
-      mapping = {-1:"red", 1:"blue", 0 :"green"} # Values of 0 are unlikely but possible
-      my_cmap = [mapping[xi] for xi in value[:,0].numpy()] # assign colours to values for each data point
-      plt.scatter(X[:,0].numpy(), X[:,1].numpy(), c=my_cmap ) 
+      mapping = {-1:"red", 1:"blue", 0 :"green"}             # Values of 0 are unlikely but possible
+      my_cmap = [mapping[xi] for xi in value[:,0].numpy()]   # Assign colours to values for each data point
+      plt.scatter(X[:,0].numpy(), X[:,1].numpy(), c=my_cmap)
+      plt.title(self.kind)
+      plt.xlabel("X1")
+      plt.ylabel("X2")
+      plt.axhline(0, c='gray', ls='dashed')  # Add lines on axes to make it easier to see quadrants:
+      plt.axvline(0, c='gray', ls='dashed') 
+
+      labels = [Line2D([0], [0], marker = 'o', c='blue', label='True'),  # Add custom legend 
+                Line2D([0], [0], marker = 'o', c='red', label='False')]
+
+      plt.legend(handles=labels, loc='upper right')
       plt.show()
-
-
-XNOR = Dataset(
-# Sampling parameters
-   100,          # Number of samples to be drawn
-   (-5.0, 5.0),  # Bounds of interval
-   "XNOR"        # Type of logic gate
-)
-
-print(XNOR.plot())
-
